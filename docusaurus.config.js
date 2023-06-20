29703ea053ab89d0ded1a4ddbc5ff7e0c69da0b6
Chris Johnson <chris.johnson@senecacollege.ca>
2023-06-20 14:30:59 -0400

    Initial course code change from template copy.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,19 +5,19 @@ const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'OER Course Title',
-  tagline: 'OER Tag/Keyword',
+  title: 'SPO600 - Software Portability and Optimization',
+  tagline: 'SPO600',
   url: 'https://Seneca-ICTOER.github.io/',
-  baseUrl: '/OERTemplate/',
+  baseUrl: '/SPO600/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'Seneca-ICTOER',
-  projectName: 'OERTemplate',
+  projectName: 'SPO600',
   themeConfig: {
     navbar: {
-      title: 'OER Course Title',
+      title: 'SPO600 - Software Portability and Optimization',
       logo: {
         alt: 'Seneca College',
         src: 'img/logo.svg',
@@ -31,7 +31,7 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'OER Course Title',
+          title: 'SPO600 - Software Portability and Optimization',
           items: [
             {
               label: 'Contents',
@@ -57,7 +57,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/Seneca-ICTOER/OERTemplate/tree/main',
+          editUrl: 'https://github.com/Seneca-ICTOER/SPO600/tree/main',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
